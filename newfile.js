b079c3e3f207185bf61e8529997fc4688979fedd
Haroon Tahir <haroon152018@gmail.com>
2024-07-04 16:03:20 +0500

    test

--- newfile.js
+++ newfile.js
@@ -1,4 +1,20 @@
 console.log("Hi!");
 console.log("test");
+<<<<<<< Updated upstream
+=======
 
 console.log(Hey);
+console.log();
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
